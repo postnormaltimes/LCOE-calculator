e253b301cbd1899061c2a6b719a3572eded8faaf
postnormaltimes <125906197+postnormaltimes@users.noreply.github.com>
2023-02-26 11:09:04 +0100

    Update LCOE program.py

--- LCOE program.py
+++ LCOE program.py
@@ -1,19 +1,62 @@
 import math
 
+# Constants
+lifetime = 60
+calibration = 8760 / 7446
+designs = {
+    "EPR": {"capacity": 1650, "construction_cost": 4013, "fuel_cycle": 70, "fixed_operations": 96},
+    "ABWR": {"capacity": 1152, "construction_cost": 3963, "fuel_cycle": 103, "fixed_operations": 173},
+    "APR1400": {"capacity": 1377, "construction_cost": 2157, "fuel_cycle": 70, "fixed_operations": 124},
+    "AP1000": {"capacity": 1100, "construction_cost": 4250, "fuel_cycle": 70, "fixed_operations": 68},
+    "VVER": {"capacity": 1122, "construction_cost": 2271, "fuel_cycle": 37, "fixed_operations": 76},
+    "CAP1400": {"capacity": 1400, "construction_cost": 2500, "fuel_cycle": 75, "fixed_operations": 177},
+    "PHWR": {"capacity": 700, "construction_cost": 2778, "fuel_cycle": 70, "fixed_operations": 160}
+}
+
 # Get user inputs
-construction_cost = float(input("Overnight construction costs per kW: "))
+while True:
+    design_choice = input("Would you like to select a design? (y/n): ")
+    if design_choice.lower() == 'y':
+        print("Available designs: ", ", ".join(designs.keys()))
+        selected_design = input("Enter the name of the selected design: ")
+        if selected_design in designs:
+            design = designs[selected_design]
+            capacity = design["capacity"]
+            construction_cost = design["construction_cost"]
+            fuel_cycle = design["fuel_cycle"]
+            fixed_operations = design["fixed_operations"]
+            break
+        else:
+            print("Invalid design selection, please try again.")
+    elif design_choice.lower() == 'n':
+        # Show available designs' characteristics
+        print("Available designs and their characteristics:")
+        for design, values in designs.items():
+            print(f"{design}:")
+            print(f"\tCapacity: {values['capacity']} MW")
+            print(f"\tConstruction Cost: €{values['construction_cost']}/kW")
+            print(f"\tFuel Cycle at 85% CF: €{values['fuel_cycle']}/kW")
+            print(f"\tFixed Operations: €{values['fixed_operations']}/kW")
+        # Get user inputs
+        capacity = float(input("Power plant capacity in MW: "))
+        construction_cost = float(input(f"Overnight construction costs (€/kW): "))
+        fuel_cycle = float(input(f"Annual front and back-end fuel costs at 85% CF (€/kW): "))
+        fixed_operations = float(input(f"Annual fixed operations and management costs (€/kW): "))
+        break
+    else:
+        print("Invalid input, please try again.")
+
 discount_rate = float(input("Discount rate: "))
 escalation_rate = float(input("Escalation rate: "))
 construction_time = int(input("Construction lead time in years: "))
 utilization_hours = float(input("Power plant utilization hours in a year: "))
-capacity = float(input("Power plant capacity in MW: "))
-lifetime = int(input("Power plant lifetime in years: "))
-fixed_operations = float(input("Annual fixed operations and management costs per kW: "))
-fuel_cycle = float(input("Annual front and back-end fuel costs per kW (at 85% capacity factor): "))
 
-# Calculate CAPITAL
+# Constants
+lifetime = 60
 kW_MW = 1000 * capacity
 calibration = utilization_hours / 7446
+
+# Calculate CAPITAL
 recovery_factor = (discount_rate * math.pow(1 + discount_rate, lifetime)) / (math.pow(1 + discount_rate, lifetime) - 1)
 capital_costs = (construction_cost * kW_MW / construction_time) * sum([math.pow(1 + escalation_rate, t + 0.5) * math.pow(1 + discount_rate, construction_time - t) for t in range(0, construction_time)])
 annual_capital_charge = capital_costs * recovery_factor
