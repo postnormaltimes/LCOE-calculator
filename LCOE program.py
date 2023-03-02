b763bf4640ffd3b96a79138139c145b40ab99ceb
postnormaltimes <125906197+postnormaltimes@users.noreply.github.com>
2023-03-02 15:49:30 +0100

    Update LCOE program.py

--- LCOE program.py
+++ LCOE program.py
@@ -13,26 +13,34 @@ designs = {
 
 # Get user inputs
 while True:
-    design_choice = input("Would you like to select a design? (y/n): ")
-    if design_choice.lower() == 'y':
-        print("Available designs: ", ", ".join(designs.keys()))
-        selected_design = input("Enter the name of the selected design: ")
-        if selected_design in designs:
-            design = designs[selected_design]
-            overnight_cost = design["overnight_cost"]
-            fuel_cycle = design["fuel_cycle"]
-            fixed_operations = design["fixed_operations"]
-            break
-        else:
-            print("Invalid design selection, please try again.")
-    elif design_choice.lower() == 'n':
-        # Show available designs' characteristics
+    # Available designs' characteristics
+    show_char = input("Would you like to see available designs and their characteristics? (y/n): ")
+    if show_char.lower() == 'y':
         print("Available designs and their characteristics:")
         for design, values in designs.items():
             print(f"{design}:")
             print(f"\tConstruction Cost: €{values['overnight_cost']}/kW")
             print(f"\tFuel Cycle at 85% CF: €{values['fuel_cycle']}/kW")
             print(f"\tFixed Operations: €{values['fixed_operations']}/kW")
+        design_choice = input("Would you like to select a design? (y/n): ")
+        if design_choice.lower() == 'y':
+            print("Available designs: ", ", ".join(designs.keys()))
+            selected_design = input("Enter the name of the selected design: ")
+            if selected_design in designs:
+                design = designs[selected_design]
+                overnight_cost = design["overnight_cost"]
+                fuel_cycle = design["fuel_cycle"]
+                fixed_operations = design["fixed_operations"]
+                break
+            else:
+                print("Invalid design selection, please try again.")
+        elif design_choice.lower() == 'n':
+            # Get user inputs
+            overnight_cost = float(input(f"Overnight construction costs (€/kW): "))
+            fuel_cycle = float(input(f"Annual front and back-end fuel costs at 85% CF (€/kW): "))
+            fixed_operations = float(input(f"Annual fixed operations and management costs (€/kW): "))
+            break
+    elif show_char.lower() == 'n':
         # Get user inputs
         overnight_cost = float(input(f"Overnight construction costs (€/kW): "))
         fuel_cycle = float(input(f"Annual front and back-end fuel costs at 85% CF (€/kW): "))
@@ -58,7 +66,6 @@ escalated_cost = sum((overnight_cost / construction_time) * (math.pow(1 + escala
 investment_cost = sum((escalated_cost / construction_time) * (math.pow(1 + discount_rate, construction_time + 1 - t)) for t in range(1, construction_time + 1))
 annual_capital_charge = investment_cost * kW_MW * recovery_factor
 CAPITAL = annual_capital_charge / utilization_hours
-print(f"Total investment cost: €{investment_cost:.2f}/kW")
 
 # Calculate FOM
 FOM = fixed_operations * kW_MW / utilization_hours
@@ -74,4 +81,5 @@ DECOMMISSIONING = ((overnight_cost * 0.15 * kW_MW * 0.01) / (math.pow(1.01, life
 
 # Calculate LCOE
 LCOE = CAPITAL + FOM + VOM + FUEL + DECOMMISSIONING
-print(f"LCOE = €{CAPITAL:.2f}/MWh + €{FOM:.2f}/MWh + €{VOM:.2f}/MWh + €{FUEL:.2f}/MWh + €{DECOMMISSIONING:.2f}/MWh = €{LCOE:.2f}/MWh")
+print(f"Total investment cost: €{investment_cost:.2f}/kW")
+print(f"Levelized cost of electricity: €{CAPITAL:.2f}/MWh + €{FOM:.2f}/MWh + €{VOM:.2f}/MWh + €{FUEL:.2f}/MWh + €{DECOMMISSIONING:.2f}/MWh = €{LCOE:.2f}/MWh")
